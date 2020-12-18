d8d4420ed700d7a58891b5228d92b6aa61f370ce
Samuel Urias <msamuelurias@gmail.com>
2020-12-17 22:49:41 -0600

    Remove reportWebVitals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,9 @@ import '../global.css'
 
 import CartProvider from '@store/Cart'
 
-export function reportWebVitals(metric) {
-  console.log(metric)
-}
+// export function reportWebVitals(metric:any) {
+//   console.log(metric)
+// }
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   // Aditional props
